c506642aa9359fbbf3cacb8c971c7a34afacc447
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-10-17 16:56:37 +0200

    Login form

---
+++ src/components/Login.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="register">
+    <form
+      @submit.prevent="loginUser"
+      class="register__form"
+    >
+      <h2 class="register__title">Login</h2>
+
+      <label class="label">
+        <span class="label__title">Email</span>
+        <input
+          class="label__input"
+          type="text"
+          v-model="user.email"
+        >
+      </label>
+
+      <label class="label">
+        <span class="label__title">Password</span>
+        <input
+          class="label__input"
+          type="password"
+          v-model="user.password"
+        >
+      </label>
+
+      <button class="register__submit" type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    loginUser() {
+      console.log(this.user);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+  @import "../scss/colors.scss";
+  @import "../scss/mixins/common.scss";
+
+  .register {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background: $blue-violet-gradient;
+
+    &__form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 40%;
+      height: auto;
+      min-height: 300px;
+      padding: 50px 0;
+      background: #FFF;
+      border-radius: 10px;
+    }
+
+    &__title {
+      margin-bottom: 50px;
+      font-size: 35px;
+    }
+
+    &__submit {
+      @include button(200px, 50px);
+
+      margin: 40px 0 20px;
+    }
+
+    & .label {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      position: relative;
+      width: 80%;
+      margin: 25px 0;
+      overflow: hidden;
+
+      &::after {
+        display: block;
+        content: '';
+        width: 100%;
+        height: 2px;
+        position: absolute;
+        bottom: 0;
+        left: -100%;
+        background: $blue-violet-gradient;
+        transition: .5s;
+      }
+
+      &::before {
+        display: block;
+        content: '';
+        width: 100%;
+        height: 2px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        background: #CCC;
+        transition: 1s;
+      }
+
+      &:focus-within::after {
+        left: 0;
+      }
+
+      &__title {
+        width: 100%;
+        text-align: left;
+        font-size: 13px;
+        text-transform: uppercase;
+        color: #999;
+      }
+
+      &__input {
+        width: 100%;
+        height: 40px;
+        border: none;
+        font-size: 17px;
+        color: #333;
+      }
+    }
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/Home.vue';
 import Register from '@/components/Register.vue';
+import Login from '@/components/Login.vue';
 import NotFound from '@/views/NotFound.vue';
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: Register,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/:catchAll(.*)',
